ae55aebee08d1e3d4e3618b1849bdc52b102f183
Charbel Merhej <79315139+charbelmerhej@users.noreply.github.com>
2022-08-16 11:42:49 +0400

    Gw1 - post-Done

--- fantasy.py
+++ fantasy.py
@@ -964,7 +964,7 @@ st.header("Weekly Player Stats")
 st.dataframe(weekly_df)
 
 st.header("Standings")
-updated_points = True
+updated_points = False
 if not updated_points:
     old_standings_df = pd.read_csv("standings.csv", index_col=0)
     st.dataframe(old_standings_df)
