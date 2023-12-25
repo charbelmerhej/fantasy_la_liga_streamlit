bbeded367db1acfaf9ffc6719ef4d68f2b46f14c
Charbel Merhej <79315139+charbelmerhej@users.noreply.github.com>
2023-12-25 19:36:31 +0400

    GW18 - PostDone

--- fantasy_v2.py
+++ fantasy_v2.py
@@ -15,7 +15,7 @@ hidden_color = "black"
 weekly_df = pd.read_csv("fantasy_weekly_v2.csv", index_col=0)
 standings_df = pd.read_csv("standings_v2.csv", index_col=0)
 
-updated_points = True
+updated_points = False
 
 
 #######################################################################################################################
@@ -24,8 +24,8 @@ updated_points = True
 charbel = [{
     "G1": "Iñaki Peña",
     "D1": "Axel Witsel",
-    "D2": "Daniel Vivian",
-    "D3": "Jules Koundé",
+    "D2": "Jules Koundé",
+    "D3": "Daniel Vivian",
     "D4": "Nacho Fernández",
     "M1": "Jude Bellingham",
     "M2": "Nico Williams",
